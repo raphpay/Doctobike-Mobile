4bb2b1408495e4b3ddcfb7152e04db5cccd2f39e
raphpay <raphpay.dev@gmail.com>
2025-10-11 10:18:18 +0400

    Fix: Correct import #3

--- src/features/dashboard/screens/DashboardScreen.tsx
+++ src/features/dashboard/screens/DashboardScreen.tsx
@@ -1,4 +1,4 @@
-import { signOut } from "@/src/lib/signOut";
+import { signOut } from "@/src/features/auth/api/signOut";
 import React from "react";
 import { Text, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
